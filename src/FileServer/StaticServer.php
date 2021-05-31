5da093fd1efbdf02666c3a9dc3140ae914a3b064
Gergely Laborci <gergely@laborci.hu>
2021-05-31 13:23:19 +0200

    minor bugfix

--- src/FileServer/StaticServer.php
+++ src/FileServer/StaticServer.php
@@ -1,13 +1,11 @@
 <?php namespace Atomino\Mercury\FileServer;
 
-
-
 use Atomino\Mercury\Router\Router;
 
 class StaticServer {
 	public static function route(Router $router, string $pattern, string $path){
 		$router(method: 'GET', path: $pattern)
-			?->pipe(FileLocator::setup($path))
+			?->pipe(...FileLocator::setup($path))
 		     ->pipe(FileServer::class);
 	}
 }
